eb7935f22f830ff84b6ff6e6916fa8e0af95bc7d
Nitharshan Karthigaseu <nitharshan1993@hotmail.com>
2020-12-17 13:43:44 +0100

    Add project files.

---
+++ ConsoleApp4GitTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp4GitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
